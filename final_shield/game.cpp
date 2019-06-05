029f05f29829929c099147de2bc9058ce0e9c638
jeremie.robles@mainbot.me <jeremierobles@gmx.fr>
2019-06-05 22:16:13 +0200

    add first demo to add player in game_master

--- final_shield/game.cpp
+++ final_shield/game.cpp
@@ -1,42 +1,78 @@
 #include "core_header.h"
 
-
 extern unsigned char terminal_adress;
 
-void   game_master(void)
+void   game_master(void)                // add gamers in room
 {
-    trame_t   trame;
-    int8_t  i;
-    int8_t  j;
-    int8_t  bt1, bt2, bt3;
-    char    tempo[32];
-    char    text[32];
-    char    players[5][3];
+    trame_t   trameS;
+    trame_t   trameR;
+
+    int8_t            bt1, bt2, bt3;
+    int8_t            cpt_players, kill_trame;
+    char              network[4];
+    unsigned char     players[10];     // 10 adress of players
+
+    players[0]          = ADRESS_MASTER;
+    cpt_players         = 1;    // master is a player too
+    kill_trame          = 0;
     
-    j         = 0;
-    i         = 1;
-    tempo[0]  = '\0';
-    text[0]   = '\0';
-    terminal_adress = 255;
+    trameR.data[0][0]   = '\0';
+    terminal_adress     = ADRESS_MASTER;
+    network[0]          = terminal_adress;
+    network[1]          = ADRESS_BRODCAST;          // default guy
+    network[2]          = '\0';
           
     lcd.clear();
-    
     lcd.set_Cursor(0,0);
     lcd.print(" *** MASTER ***");
-
+    lcd.set_Cursor(0, 1);
+    lcd.printstr("players: ");
+    lcd.set_Cursor(10, 1);
+    lcd.print(cpt_players);
+          
     radio_init_receive("00001");
-    while (i)
+    do
     {
         key_loop(&bt1, &bt2, &bt3);
-        //radio_receive(tempo);
-
-  
-        if (bt3) 
-        {
-          i = 0;
+        radio_receive(&trameR);
+          
+        if (trameR.data[0][0] != '\0')        // we have a data
+        { 
+          if ((strcmp( JOIN_REQUEST, trameR.data[0]) == 0))   // we have a join request 
+          {
+              radio_init_sender("00001");           // change mode sender
+              network[1] = trameR.adress;
+            
+               for (int8_t b = 0; b < 10; b++)
+               {
+                  if (trameR.adress == players[b] || cpt_players > 10)   // error player
+                  {                                                     // send conflit adress...
+                    kill_trame = 1;
+                    create_trame(&trameS, network, CONFLIT_ADRESS, END_COMMAND);
+                  }
+               }
+             
+               if (!kill_trame)                                     // if we have no problem
+               {                                                    // accept request
+                  players[cpt_players++] = trameR.adress;           
+                  create_trame(&trameS, network, JOIN_VALIDATION, END_COMMAND);
+               }
+               else
+                kill_trame = 0;                   // reset kill accept
+              radio_send(&trameS);                // send trame
+              radio_init_receive("00001");        // change mode receive
+          }
+          
+          trameR.data[0][0]  = '\0';      // reset trame receive
+          lcd.set_Cursor(0, 1);
+          lcd.printstr("players: ");
+          lcd.set_Cursor(10, 1);
+          lcd.print(cpt_players);
         }
-        lcd.backlight();    // set light ON (in loop, shit code..)  
-    }
+        
+        lcd.backlight();    // set light ON (in loop, shit code..)    
+    } while (!bt3);
+    
     terminal_adress = 5;    // restart default adress
     lcd.clear();
 }
